0af78b3871a1f45ab33fc0ec709afbc30897c030
mathe <matheusmen58@gmail.com>
2025-12-20 20:51:34 -0300

    HTML atualização

--- src/main/resources/templates/javascript/cadastro_user.js
+++
@@ -1,9 +0,0 @@
-document.getElementById("formCadastro").addEventListener("submit",function(e){
-    e.preventDefault();
-
-    const dados={
-        nomeUsuario: document.querySelector('input[name="nome"]').value,
-        email:document.querySelector('input[name="email"]').value,
-        senha
-    }
-})
