063ba2c4fb62652b19ebb2c1ba3f585b277d34a3
mathe <matheusmen58@gmail.com>
2026-02-07 09:12:13 -0300

    Atualização de Recursos.

--- src/main/java/LeadBio/Biblioteca/Repository/FuncionarioRepository.java
+++
@@ -1,7 +0,0 @@
-package LeadBio.Biblioteca.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FuncionarioRepository extends JpaRepository<FuncionarioDB, Long> {
-
-}
